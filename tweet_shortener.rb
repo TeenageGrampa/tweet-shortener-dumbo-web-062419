887fcafc7f9840d25ccd068240a871012e99464f
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-19 20:46:41 +0000

    Automatically backed up by Learn

---
+++ tweet_shortener.rb
@@ -0,0 +1,14 @@
+def dictionary
+  dictionary = {
+    "too" => "2",
+    "to" => "2",
+    "two" =>"2",
+    "four" => "4",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
