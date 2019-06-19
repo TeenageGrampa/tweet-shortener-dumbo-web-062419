63e7ca3a3cb8d4e8b63b85ecacef3b6bc243b801
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-19 20:48:20 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,3 +12,29 @@ def dictionary
   }
 end
 
+def word_substituter(tweet)
+  words = tweet.split(" ")
+  new_tweet = []
+
+  words.each do |word|
+    case word.downcase
+    when "hello"
+      new_tweet << "hi"
+    when "to", "two", "too"
+      new_tweet << "2"
+    when "for", "four" 
+      new_tweet << "4"
+    when "be" 
+      new_tweet << "b"
+    when "you" 
+      new_tweet << "u"
+    when "at" 
+      new_tweet << "@" 
+    when "and" 
+      new_tweet << "&"
+    else
+      new_tweet << word
+    end
+  end
+  new_tweet.join(" ")
+end
